4e1981f77390f0f3a5b856e1426f336927662e6c
Benny O <opw0011@gmail.com>
2016-09-14 11:32:58 +0800

    Folder

--- src/base/NoteBook.java
+++ src/base/NoteBook.java
@@ -6,7 +6,47 @@ public class NoteBook {
 	
 	private ArrayList<Folder> folders;
 	public NoteBook() {
+		folders = new ArrayList<Folder>();
+	}
+	
+	public boolean createTextNote(String folderName, String title) {
+		TextNote note = new TextNote(title);
 		
+		return insertNote(folderName, note);
+	}
+	
+	public boolean createImageNote(String folderName, String title) {
+		ImageNote note = new ImageNote(title);
+		return insertNote(folderName, note); 
+	}
+
+	public ArrayList<Folder> getFolders() {
+		return folders;
+	}
+	
+	private boolean insertNote(String folderName, Note note) {
+		Folder f = null;
+		for(Folder f1 : folders) {
+			if(f1.getName().equals(folderName)) {
+				f = f1;
+				break;
+			}			
+		}
+		
+		if(f == null) {
+			f = new Folder(folderName);
+			folders.add(f);
+		}
+		
+		for(Note n : f.getNotes()) {
+			if(n.equals(note)) {
+				System.out.println("Creating note " + note.getTitle() + " under fodler " + folderName + " failed");
+				return false;
+			}
+		}
+		
+		f.addNote(note);		
+		return true;		
 	}
 	
 	
